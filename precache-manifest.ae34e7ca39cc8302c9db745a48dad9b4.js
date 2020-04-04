65b4a4826598d55a54040db9fb4990325c82c70a
gux <gux.vega@gmail.com>
2020-04-04 16:15:29 -0600

    Updates

---
+++ precache-manifest.ae34e7ca39cc8302c9db745a48dad9b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee5ea06b0c30762e72acd1628df3c5e6",
+    "url": "/Base-React-Application/index.html"
+  },
+  {
+    "revision": "0217e0fbb92d7b0f747b",
+    "url": "/Base-React-Application/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "50bfb66677afcb097cf8",
+    "url": "/Base-React-Application/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0217e0fbb92d7b0f747b",
+    "url": "/Base-React-Application/static/js/2.f68723af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Base-React-Application/static/js/2.f68723af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50bfb66677afcb097cf8",
+    "url": "/Base-React-Application/static/js/main.586b79d9.chunk.js"
+  },
+  {
+    "revision": "4ee6133230fead538dbf",
+    "url": "/Base-React-Application/static/js/runtime-main.de0e1d7a.js"
+  }
+]);
